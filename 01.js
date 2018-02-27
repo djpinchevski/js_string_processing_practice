bad8ed9aefc3bff8258422c0e0eaa8ae5d6b9c12
Dave Pinchoff <dpinchoff@gmail.com>
2018-02-27 13:45:31 +0200

    first commit

---
+++ 01.js
@@ -0,0 +1,6 @@
+// Create a variable named firstName and set it equal to your first name. Set another variable named lastName to your last name. Join the two together, separated by a space, and store the result in a variable named fullName. Log the value of fullName.
+
+let firstName = 'Dave';
+let lastName = 'Pinchevski';
+let fullName = [firstName, lastName].join(' ');
+console.log(fullName);
